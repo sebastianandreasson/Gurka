e34d9fc280a6fb7deb508e6e368e3f336ba7a77f
Sebastian Andreasson <xandse@ml-200403-001.local>
2020-05-08 21:54:45 +0200

    update gif generation and displaying on web

--- web/server/index.js
+++ web/server/index.js
@@ -5,6 +5,7 @@ const PORT = process.env.PORT ? process.env.PORT : 4000
 const GIFEncoder = require('gifencoder')
 const sharp = require('sharp')
 const fs = require('fs')
+const path = require('path')
 const fse = require('fs-extra')
 const express = require('express')
 const cors = require('cors')
@@ -25,16 +26,34 @@ const gif_height = Math.floor(height / 4)
 app.get('/', (req, res) => {
   res.send({ gur: 'ka' })
 })
+app.get('/data/*', (req, res) => {
+  const filePath = req.params[0]
+  let url = path.join(`${process.cwd()}/data/${filePath}`)
+  res.sendFile(url)
+})
 
-app.get('/gurkor', async (req, res) => {
-  fs.readdir(`${DATA_DIR}/gurkor`, (err, files) => {
-    files.forEach((file) => {
-      console.log(file)
-    })
-    generateGif(files)
-    res.send(files)
+app.get('/gif', async (req, res) => {
+  const { date } = req.query
+  if (!date)
+    return res.send({ error: 'date required in query ( ?date=YYYY-MM-DD )' })
+
+  fs.readdir(`${DATA_DIR}/gurkor/${date}`, async (error, files) => {
+    if (error) return res.send({ error })
+    const gif = await generateGif(date, files)
+
+    res.send(gif)
   })
 })
+app.get('/gifs', async (req, res) => {
+  fs.readdir(`${DATA_DIR}/gifs`, async (error, files) => {
+    if (error) return res.send({ error })
+
+    res.send(
+      files.filter((f) => f !== 'test.txt').map((f) => `/data/gifs/${f}`)
+    )
+  })
+})
+
 app.post('/gurkor', async (req, res) => {
   res.send({ ok: true })
   const dir = `${DATA_DIR}/gurkor/${moment().format('YYYY-MM-DD')}`
@@ -49,7 +68,7 @@ app.post('/gurkor', async (req, res) => {
 app.listen(PORT, () => console.log(`listening on port ${PORT}`))
 
 const getImageDataFromFilePath = (path) => {
-  return sharp(`./data/gurkor/${path}`)
+  return sharp(path)
     .resize(gif_width, gif_height)
     .joinChannel(Buffer.alloc(gif_width * gif_height, 255), {
       raw: { channels: 1, width: gif_width, height: gif_height },
@@ -58,19 +77,26 @@ const getImageDataFromFilePath = (path) => {
     .toBuffer()
 }
 
-const generateGif = async (imagePaths) => {
-  console.log(imagePaths)
+const generateGif = async (date, imagePaths) => {
+  console.log('generateGif', date, imagePaths)
+  const gifPath = `${DATA_DIR}/gifs/${date}.gif`
+
   const encoder = new GIFEncoder(gif_width, gif_height)
   const frames = await Promise.all(
-    imagePaths.map((path) => getImageDataFromFilePath(path))
+    imagePaths
+      .map((imgPath) => `${DATA_DIR}/gurkor/${date}/${imgPath}`)
+      .map((path) => getImageDataFromFilePath(path))
   )
-  encoder.createReadStream().pipe(fs.createWriteStream('gurka.gif'))
+  encoder.createReadStream().pipe(fs.createWriteStream(gifPath))
   encoder.start()
   encoder.setRepeat(0)
-  encoder.setDelay(250)
+  encoder.setDelay(100)
   encoder.setQuality(10)
   frames.forEach((frame) => {
     encoder.addFrame(frame)
   })
   encoder.finish()
+  return gifPath
 }
+
+fse.outputFile(`${DATA_DIR}/gifs/test.txt`, 'test', 'base64')
--- web/src/index.js
+++ web/src/index.js
@@ -1,8 +1,84 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import ReactDOM from 'react-dom'
+import styled from 'styled-components'
+import axios from 'axios'
+
+const API_URL = 'https://gurkapi.sebastianandreasson.com'
+
+const Container = styled.div`
+  margin: 0 auto;
+  width: 80%;
+
+  @media (max-width: 540px) {
+    width: 95%;
+  }
+`
+
+const Title = styled.h1`
+  @media (max-width: 540px) {
+    text-align: center;
+  }
+`
+
+const ImageList = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+`
+
+const Image = styled.div`
+  margin: 15px;
+  display: flex;
+  flex-direction: column;
+
+  @media (max-width: 540px) {
+    margin: 0;
+  }
+
+  > span {
+    width: 100%;
+    padding: 5px;
+    font-size: 24px;
+    font-weight: bold;
+    text-align: center;
+  }
+
+  > img {
+    border-radius: 8px;
+    max-width: 100%;
+    width: 480px;
+  }
+`
+
+const sliceName = (fileName) => {
+  return fileName.replace('/data/gifs/', '').replace('.gif', '')
+}
 
 const App = () => {
-  return <div>🥒</div>
+  const [images, setImages] = useState([])
+
+  useEffect(() => {
+    async function getImages() {
+      const response = await axios.get(`${API_URL}/gifs`)
+      const _images = await response.data
+
+      setImages(_images)
+    }
+
+    getImages()
+  }, [])
+  return (
+    <Container>
+      <Title>🥒 Gurkor 🥒</Title>
+      <ImageList>
+        {images.map((img) => (
+          <Image>
+            <span>{sliceName(img)}</span>
+            <img src={`${API_URL}${img}`}></img>
+          </Image>
+        ))}
+      </ImageList>
+    </Container>
+  )
 }
 
 ReactDOM.render(
